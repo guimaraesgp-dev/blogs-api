480325517729177129503fdb8a5eda2357d066da
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-05-19 14:53:11 -0300

    REQ 2

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+      'User',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER,
+        },
+        displayName: {
+          type: DataTypes.STRING,
+        },
+        email: {
+          type: DataTypes.STRING,
+        },
+        password: {
+          type: DataTypes.STRING,
+        },
+        image: {
+          type: DataTypes.STRING,
+        },
+      },
+      {
+        tableName: 'users',
+        timestamps: false,
+        underscored: true,
+      });
+    return User;
+  };
