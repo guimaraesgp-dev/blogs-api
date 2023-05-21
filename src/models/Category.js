784501fda9dfa862b39406a8606049b64ec97ff3
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-05-21 13:25:16 -0300

    REQ 7

---
+++ src/models/Category.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define(
+      'Category',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER,
+        },
+        name: {
+          type: DataTypes.STRING,
+        },
+      },
+      {
+        tableName: 'categories',
+        timestamps: false,
+        underscored: true,
+      });
+    return Category;
+  };
